28b2024e1daa694995b9e471a502ca1cf8350207
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-15 08:10:06 +0700

    commit 2

---
+++ BankTransferConfig.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+public class BankTransferConvig { 
+    public int lang { get; set; }
+    public Transfer transfer { get; set; }
+    public String[] methods  { get; set; }
+    public Confirmation confirmation { get; set; } 
+}
+
+public class Transfer {
+    public int threshold { get; set; }
+    public int low_fee { get; set; }
+    public int high_fee { get; set; }
+}
+
+public class Confirmation { 
+    public String en { get; set; }
+    public String id { get; set; }
+}
+
+public class UIConfig {
+    public BankTransferConvig config;
+
+    public const String filePath = @"bank_transfer_config.json";
+
+    public UIConfig()
+    {
+        try
+        {
+            ReadConfigFile();
+        }
+        catch (Exception)
+        {
+            SetDefault();
+            WriteNewConfigFile();
+        }
+    }
+
+    private BankTransferConvig ReadConfigFile()
+    {
+        String configJsonData = File.ReadAllText(filePath);
+        config = JsonSerializer.Deserialize<BankTransferConvig>(configJsonData); return config;
+    }
+
+    private void SetDefault() {
+        "lang": "en”,
+        "transfer": {
+                "threshold": 2500000,
+            "low_fee": 6500,
+            "high_fee": 15000
+        },
+        "methods": ["“RTO", "(real-time)”", "“SKN”", "“RTGS”", "“BI", "FAST”"],
+        "confirmation": {
+            "en": "yes”,,
+            "id": "ya”
+        }
+    }
+
+    private void WriteNewConfigFile()
+    {
+        JsonSerializerOptions options = new JsonSerializerOptions()
+        {
+            WriteIndented = true
+        };
+        String jsonString = JsonSerializer.Serialize(config, options);
+        File.WriteAllText(filePath, jsonString);
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.ComponentModel.Design;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BankTransferConvig config = new BankTransferConvig();
+        Console.WriteLine();
+
+        if (config BankTarnsferConfig lang == "en"){
+            Console.WriteLine("Please insert the amount of money to transfer: ");
+        }else if (config banktransferconfig lang == "id"){
+            Console.WriteLine("Masukkan jumlah uang yang akan di-transfer:");
+        }
+
+        if (Transfer )
+    }
+}
